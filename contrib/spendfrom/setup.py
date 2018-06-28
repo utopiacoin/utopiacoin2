e1e71b6083a9b10fd9bfdd9558b6f830cd0f0b28
utopiacoin <40309430+utopiacoin@users.noreply.github.com>
2018-06-28 17:12:39 -0400

    Add files via upload

--- contrib/spendfrom/setup.py
+++ contrib/spendfrom/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
-setup(name='btcspendfrom',
+setup(name='NCNspendfrom',
       version='1.0',
-      description='Command-line utility for bitcoin "coin control"',
+      description='Command-line utility for newcoin "coin control"',
       author='Gavin Andresen',
-      author_email='gavin@bitcoinfoundation.org',
+      author_email='gavin@newcoinfoundation.org',
       requires=['jsonrpc'],
       scripts=['spendfrom.py'],
       )
